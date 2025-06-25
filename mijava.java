a76b14c2732d299cd55e9691f0ac62ec26d520f9
Josecarranza19 <jose.carranza978@std.uni.edu.ni>
2025-06-24 18:54:08 -0600

    mi primer cambio confirmado

---
+++ mijava.java
@@ -0,0 +1,14 @@
+public class mijava {
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 5;
+        int suma = a+b;
+        String nombre = "estudiante";
+       
+
+        System.out.println("hola"+ nombre + "!");
+        System.out.println("la suma de a+b " + suma);
+       
+    }
+}
